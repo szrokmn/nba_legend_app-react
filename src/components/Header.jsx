540a66808289e68bf194e71169131d27fa63c1da
Sezer Ökmen <szrokmn34@gmail.com>
2023-02-28 00:00:07 +0300

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
-import './Header.scss';
-
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import nbaLogo from "../assets/nba-logo.png";
 const Header = () => {
   return (
-    <div className="header">
-            <span className="text-shadow">Who Are You ?</span>
-        </div>
-  )
-}
-
-export default Header
+    <Container>
+      <Image src={nbaLogo} width="200px"></Image>
+      <h1 className="my-2 font-monospace display-4 fw-bold">NBA Legends</h1>
+    </Container>
+  );
+};
+export default Header;
