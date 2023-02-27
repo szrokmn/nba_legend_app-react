da8a817f9bd505c95082147d3bae99a988c57784
Sezer Ökmen <szrokmn34@gmail.com>
2023-02-28 00:03:00 +0300

    css finished

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
-import { data } from "../helpers/data";
+import { data } from "../helper/data";
 import PlayerCard from "./PlayerCard";
 import { useState } from "react";
 
